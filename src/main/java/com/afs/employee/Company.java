16483b7fab12437419da47afc95ff59c85a342f1
Marie Chow <chowma3@oocl.com>
2022-12-06 21:54:22 +0800

    feat: add getAll CompanyController

---
+++ src/main/java/com/afs/employee/Company.java
@@ -0,0 +1,24 @@
+package com.afs.employee;
+
+public class Company {
+
+    private final Integer id;
+    private String name;
+
+    public Company(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/afs/employee/CompanyRepository.java
@@ -0,0 +1,20 @@
+package com.afs.employee;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CompanyRepository {
+    List<Company> companyList;
+
+    public CompanyRepository(List<Company> companyList) {
+        this.companyList = companyList;
+        companyList.add(new Company(1, "spring"));
+        companyList.add(new Company(2, "boot"));
+    }
+
+    public List<Company> getAll() {
+        return this.companyList;
+    }
+}
