99fd16b836e80e57a359342b6e2b84bbacf9f77d
Wongje4 <>
2022-12-06 19:38:12 +0800

    feat: obtain companylist

---
+++ src/main/java/com/afs/employee/CompanyRepository.java
@@ -0,0 +1,28 @@
+package com.afs.employee;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class CompanyRepository {
+    private List<Company> companies = new ArrayList<>();
+
+
+    private List<Employee> employeesList1 = Arrays.asList(new Employee(1, "Lily", 20, "female", 8000),
+            new Employee(2, "Lily2", 21, "female", 8100));
+
+    private List<Employee> employeesList2 = Arrays.asList(new Employee(1, "Jenny", 20, "female", 8000),
+            new Employee(2, "Jenny2", 21, "female", 8100));
+
+    public CompanyRepository() {
+        companies.add(new Company(1,"company1",employeesList1));
+        companies.add(new Company(2,"company2",employeesList2));
+    }
+
+    public List<Company> findAll() {
+        return companies;
+    }
+}
